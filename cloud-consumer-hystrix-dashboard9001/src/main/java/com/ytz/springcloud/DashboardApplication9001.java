5968796ef3b4a90a42a38a4c4a88e1156bff6ae7
yangtz <yangtz0213@126.com>
2020-06-15 22:04:53 +0800

    hystrix-服务熔断处理 Dashboard-hystrix监控页面

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/ytz/springcloud/DashboardApplication9001.java
@@ -0,0 +1,36 @@
+package com.ytz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @ClassName: DashboardApplication9001
+ * @Description: TODO
+ * @author: basketBoy
+ * @date: 2020/6/15
+ * @Version: V1.0
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DashboardApplication9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication9001.class, args);
+    }
+
+    /**
+     * 此配置是为了服务监控而配置，与服务容错本身无观，springCloud 升级之后的坑
+     * ServletRegistrationBean因为springboot的默认路径不是/hystrix.stream
+     * 只要在自己的项目中配置上下面的servlet即可
+     * @return
+     */
+//    @Bean
+//    public ServletRegistrationBean getServlet(){
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//        return registrationBean;
+//    }
+}
--- cloud-provider-hystrix-payment8001/src/main/java/com/ytz/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/ytz/springcloud/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.ytz.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
@@ -57,4 +58,42 @@ public class PaymentService {
     public String paymentInfoTimeOutHandler(Integer id) {
         return "线程池:" + Thread.currentThread().getName() + " 系统繁忙或运行错误,请稍后重试,id:" + id + "\t" + "o(╥﹏╥)o";
     }
+
+    /**
+     * 服务熔断
+     *     open: 熔断打开，请求不在进行服务调用当前服务
+     *     half-open: 熔断半开，部分请求根据规则调用当前服务，如果当前请求成功且符合规则，可以认为当前服务恢复正常，关闭熔断
+     *     closed: 熔断关闭，不会对服务进行熔断
+      */
+    /**
+     * 在10秒窗口期中10次请求有6次是请求失败的,断路器将起作用
+     * @param id
+     * @return
+     */
+    @HystrixCommand(
+            fallbackMethod = "paymentCircuitBreakerfallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),// 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),// 时间窗口期/时间范文
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")// 失败率达到多少后跳闸
+    }
+    )
+    public String paymentCircuitBreaker(Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("*****id不能是负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + "\t" + "调用成功,流水号:" + serialNumber;
+    }
+
+    /**
+     * 服务报错：返回处理的方法
+     * @param id
+     * @return
+     */
+    public String paymentCircuitBreakerfallback(Integer id) {
+        return "id 不能负数,请稍后重试,o(╥﹏╥)o id:" + id;
+    }
+
+
 }
